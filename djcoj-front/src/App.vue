16b49d4768bf182c07fb2b8c01ff9c532bc90649
buffyyds <dengjiachengyyds@outlook.com>
2025-02-19 18:17:57 +0800

    openapi自动生成接口

--- djcoj-front/src/App.vue
+++ djcoj-front/src/App.vue
@@ -13,7 +13,7 @@ import BasicLayout from "@/layouts/BasicLayout.vue";
 import { useRouter } from "vue-router";
 import { useStore } from "vuex";
 import AccessEnum from "@/access/accessEnum";
-import {onMounted} from "vue";
+import { onMounted } from "vue";
 
 /**
  * 全局初始化函数，有全局单次调用的代码，都可以写到这里
