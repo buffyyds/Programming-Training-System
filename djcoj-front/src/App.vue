753182604a0fcb40f7fb09dd059c016546afa201
buffyyds <dengjiachengyyds@outlook.com>
2025-02-19 11:08:49 +0800

    后端重新初始化

--- djcoj-front/src/App.vue
+++ djcoj-front/src/App.vue
@@ -13,6 +13,18 @@ import BasicLayout from "@/layouts/BasicLayout.vue";
 import { useRouter } from "vue-router";
 import { useStore } from "vuex";
 import AccessEnum from "@/access/accessEnum";
+import {onMounted} from "vue";
+
+/**
+ * 全局初始化函数，有全局单次调用的代码，都可以写到这里
+ */
+const doInit = () => {
+  console.log("hello 欢迎来到我的项目");
+};
+
+onMounted(() => {
+  doInit();
+});
 
 const router = useRouter();
 const store = useStore();
